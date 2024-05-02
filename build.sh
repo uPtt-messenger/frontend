9d529b00e69717980f31689c617b6fb7763a76dd
NoobTW <noob@noob.tw>
2024-05-02 21:05:59 +0800

    chore: add build script

---
+++ build.sh
@@ -0,0 +1,39 @@
+#!/usr/bin/env bash
+set -Eeuo pipefail
+
+function build() {
+    pnpm run build
+	cp -r dist build-packages
+}
+
+function script_usage() {
+    cat << EOF
+
+Usage: $0 <build>
+EOF
+}
+
+function main() {
+    if (( $# < 1 )); then
+        script_usage
+        exit 0
+    fi
+
+    action="${1}"
+
+    local action="${1}"
+
+    case "$action" in
+      build)
+          build
+          ;;
+      *)
+          echo "Unknown action"
+          echo
+          script_usage
+          exit 1
+          ;;
+    esac
+}
+
+main "$@"
